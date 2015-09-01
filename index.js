e877e3d30e903a4b46df6bf0f1b49cfd1f548c29
pearmaster <github@jacobbrunson.com>
2015-09-01 10:49:25 -0600

    Added semicolons to lines

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-var week = /^P([0-9]+W)$/
-var dateTime = /^P(([0-9]+Y)?([0-9]+M)?([0-9]+D)?)?(T([0-9]+H)?([0-9]+M)?([0-9]+S)?)?$/
+var week = /^P([0-9]+W)$/;
+var dateTime = /^P(([0-9]+Y)?([0-9]+M)?([0-9]+D)?)?(T([0-9]+H)?([0-9]+M)?([0-9]+S)?)?$/;
 
 function extractInt (str) {
-  return parseInt(str.substring(0, str.length - 1), 10)
-}
+  return parseInt(str.substring(0, str.length - 1), 10);
+};
 
 function parsePart (mode, str) {
-  if (!str) return 0
+  if (!str) {
+    return 0;
+  }
 
-  var n = extractInt(str)
-  var id = mode + ' ' + str[str.length - 1]
+  var n = extractInt(str);
+  var id = mode + ' ' + str[str.length - 1];
 
-  if (n === 0) return 0
+  if (n === 0) {
+    return 0;
+  }
 
   switch (id) {
-    case 'time S': return n * 1000
-    case 'time M': return n * 60000
-    case 'time H': return n * 3600000
-    case 'date D': return n * 86400000
-    case 'week W': return n * 604800000
+    case 'time S': return n * 1000;
+    case 'time M': return n * 60000;
+    case 'time H': return n * 3600000;
+    case 'date D': return n * 86400000;
+    case 'week W': return n * 604800000;
     case 'date M':
     case 'date Y':
-      throw new Error('Ambiguous duration')
+      throw new Error('Ambiguous duration');
   }
 }
 
 function parseIsoDuration (str) {
-  var m
+  var m;
 
-  m = week.exec(str)
+  m = week.exec(str);
   if (m) {
-    return parsePart('week', m[1])
+    return parsePart('week', m[1]);
   }
 
-  m = dateTime.exec(str)
+  m = dateTime.exec(str);
   if (m) {
     return (
       parsePart('date', m[2]) +
@@ -42,10 +46,10 @@ function parseIsoDuration (str) {
       parsePart('time', m[6]) +
       parsePart('time', m[7]) +
       parsePart('time', m[8])
-    )
+    );
   }
 
-  throw new Error('Invalid duration')
+  throw new Error('Invalid duration');
 }
 
-module.exports = parseIsoDuration
+module.exports = parseIsoDuration;
